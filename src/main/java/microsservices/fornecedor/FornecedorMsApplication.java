54e70918cdd6f28029b82390781b2f84219dacf0
kaique <kaiquesilva49@hotmail.com>
2022-10-05 10:15:00 -0300

    registrando o serviço no eureka

--- src/main/java/microsservices/fornecedor/FornecedorMsApplication.java
+++ src/main/java/microsservices/fornecedor/FornecedorMsApplication.java
@@ -3,11 +3,13 @@ package microsservices.fornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FornecedorMsApplication {
 
 	public static void main(String[] args) {
